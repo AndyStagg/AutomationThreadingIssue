9036d21138a96ae7a2533a0d91c498f18706f845
andy stagg <atwinda@gmail.com>
2022-01-15 16:42:44 -0800

    Add project files.

---
+++ AutomationThreadingIssue.WPF/Automation.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutomationThreadingIssue.WPF
+{
+    public class AutomationBackgroundWorker
+    {
+        private ManualResetEvent manualResetEvent;
+        private Thread automationThread;
+
+        private SynchronizationContext backgroundWorkerContext;
+
+        public AutomationBackgroundWorker()
+        {
+            Thread.CurrentThread.Name = "Automation Background Worker";
+            backgroundWorkerContext = SynchronizationContext.Current;
+
+            automationThread = new Thread(this.Automation);
+            automationThread.Name = "Automation Thread";
+            automationThread.SetApartmentState(ApartmentState.STA);
+
+            manualResetEvent = new ManualResetEvent(false);
+        }
+
+        public void Begin()
+        {
+            automationThread.Start();
+
+            manualResetEvent.WaitOne();
+        }
+
+        public void Complete()
+        {
+            manualResetEvent.Set();
+            manualResetEvent.Close();
+        }
+
+        public void Automation()
+        {
+            var automationSyncContext = SynchronizationContext.Current;
+
+            AutomationMessenger.Send("Status", "Initializing", null);
+
+            // Initialize Automation Elements, etc..
+            Task.Delay(1000).Wait();
+
+            AutomationMessenger.Send("Status", "Initialized", null);
+
+            // Get user input
+            AutomationMessenger.Send("UserInput", "Please Enter Your Name", UserInputCallback);
+
+            // Wait for their response
+        }
+
+        // As you would expect, this method is being called from the WPF Thread / UI Thread
+        // How can I synchronize it back to the Automation Thread
+        private void UserInputCallback(string userInput)
+        {
+            //if (Thread.CurrentThread != automationThread)
+            //{
+            //    automationThread.Join();
+            //}
+            // Doing this blocks and kills the Automation Thread
+
+            if (string.IsNullOrWhiteSpace(userInput)) return;
+
+            AutomationMessenger.Send("Status", $"Thanks {userInput}", null);
+
+            Complete();
+        }
+    }
+
+    public static class AutomationMessenger
+    {
+        public static event AutomationMessengerEventHandler Received;
+        public static void Send(string name, string message, Action<string> callbackAction) => Received?.Invoke(null, new AutomationMessengerEventArgs(name, message, callbackAction));
+    }
+
+    public delegate void AutomationMessengerEventHandler(object sender, AutomationMessengerEventArgs e);
+
+    public class AutomationMessengerEventArgs : EventArgs
+    {
+        public string Name { get; set; }
+        public string Message { get; set; }
+        public Action<string> CallbackAction { get; set; }
+        public AutomationMessengerEventArgs(string name, string message, Action<string> callbackAction) : base()
+        {
+            Name = name;
+            Message = message;
+            CallbackAction = callbackAction;
+        }
+    }
+}
---
+++ AutomationThreadingIssue.WPF/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AutomationThreadingIssue.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Thread.CurrentThread.Name = "WPF Thread";
+
+            AutomationMessenger.Received += AutomationMessenger_Received;
+        }
+
+        private void AutomationMessenger_Received(object sender, AutomationMessengerEventArgs e)
+        {
+            switch (e.Name)
+            {
+                case "Status":
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        Status_TextBlock.Text = e.Message;
+                    });
+
+                    break;
+
+                case "UserInput":
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        AutomationInput_Grid.IsEnabled = true;
+
+                        Input_Button.Tag = e.CallbackAction;
+
+                        Status_TextBlock.Text = e.Message;
+                    });
+
+                    break;
+            }
+        }
+
+        private void Run_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Run_Button.IsEnabled = false;
+
+            var backgroundAutomationTask = new Task(() =>
+            {
+                var worker = new AutomationBackgroundWorker();
+                worker.Begin();
+            });
+
+            backgroundAutomationTask.Start();
+        }
+
+        private void Input_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Input_Button.Tag != null && Input_Button.Tag is Action<string> callbackAction)
+            {
+                callbackAction.Invoke(Input_TextBox.Text);
+            }
+        }
+    }
+}
